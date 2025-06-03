0ebc9abf86ccc07f37a1f642fcd22adf82a26833
TeaGross <tea.grossman@gmail.com>
2025-06-03 11:48:38 +0200

    feat(TodoList): implementing localstorage
    -setItem
    -getItem

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -4,17 +4,18 @@ import "./Todolist.css"
 import { TodoPresentation } from "./TodoPresentation"
 
 export const TodoList = () => {
-    const [todos, setTodos] = useState<Todo[]>([
-        new Todo(1, "Göra inlämningsuppgiften", false),
-        new Todo(2, "Handla mat", false),
-        new Todo(3, "Gör ett spread i junk journal", false),
-        new Todo(4, "Vattna blommorna", false),
-        new Todo(5, "Ta en promenad", false),
-        new Todo(6, "Se Håkan Hellström på Ullevi", false),
-        new Todo(7, "Test1", false),
-        new Todo(8, "Test2", false),
-        new Todo(9, "Test3", false),
-    ])
+    const [todos, setTodos] = useState<Todo[]>(
+        JSON.parse(localStorage.getItem("todos") || JSON.stringify([
+            new Todo(1, "Göra inlämningsuppgiften", false),
+            new Todo(2, "Handla mat", false),
+            new Todo(3, "Gör ett spread i junk journal", false),
+            new Todo(4, "Vattna blommorna", false),
+            new Todo(5, "Ta en promenad", false),
+            new Todo(6, "Se Håkan Hellström på Ullevi", false),
+            new Todo(7, "Test1", false),
+            new Todo(8, "Test2", false),
+            new Todo(9, "Test3", false),  
+     ])))
 
     const toggleDone = (id: number) => {
         setTodos(
@@ -30,6 +31,8 @@ export const TodoList = () => {
     const deleteTodo = (id : number) => {
         setTodos(todos.filter((t) => t.id !== id ))
     }
+
+    localStorage.setItem("todos", JSON.stringify(todos))
 
     return (
         <>
